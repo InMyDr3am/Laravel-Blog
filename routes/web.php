652c8e5412165a499f855547dc3a75b97ca9cac7
Fauzi Ramdhani <fauzi.thc@gmail.com>
2024-11-05 13:51:52 +0700

    UI artikel, artikel data array

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,64 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Fauzi Ramdhani',  'title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Artikel 1',
+            'author' => 'Fauzi R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Iusto et nobis soluta provident autem repellendus nemo, 
+            nesciunt delectus veritatis reprehenderit minima maxime 
+            impedit dolorum rerum ipsam repellat assumenda. 
+            Recusandae, architecto?'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Artikel 2',
+            'author' => 'Fauzi R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Iusto et nobis soluta provident autem repellendus nemo, 
+            nesciunt delectus veritatis reprehenderit minima maxime 
+            impedit dolorum rerum ipsam repellat assumenda. 
+            Recusandae, architecto?'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Artikel 1',
+            'author' => 'Fauzi R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Iusto et nobis soluta provident autem repellendus nemo, 
+            nesciunt delectus veritatis reprehenderit minima maxime 
+            impedit dolorum rerum ipsam repellat assumenda. 
+            Recusandae, architecto?'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Artikel 2',
+            'author' => 'Fauzi R',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Iusto et nobis soluta provident autem repellendus nemo, 
+            nesciunt delectus veritatis reprehenderit minima maxime 
+            impedit dolorum rerum ipsam repellat assumenda. 
+            Recusandae, architecto?'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
