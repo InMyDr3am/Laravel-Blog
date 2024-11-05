ebcb87b88c0554732ce9f13289d1e8f3860c2bdd
Fauzi Ramdhani <fauzi.thc@gmail.com>
2024-11-05 14:45:06 +0700

    buat model post, pindah post ke model dri route

---
+++ app/Models/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Artikel 1',
+                'author' => 'Fauzi R',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Iusto et nobis soluta provident autem repellendus nemo, 
+                nesciunt delectus veritatis reprehenderit minima maxime 
+                impedit dolorum rerum ipsam repellat assumenda. 
+                Recusandae, architecto?'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'judul-artikel-2',
+                'title' => 'Artikel 2',
+                'author' => 'Fauzi R',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Iusto et nobis soluta provident autem repellendus nemo, 
+                nesciunt delectus veritatis reprehenderit minima maxime 
+                impedit dolorum rerum ipsam repellat assumenda. 
+                Recusandae, architecto?'
+            ]
+        ];
+    }
+
+    public static function find($slug): array
+    {
+        // return Arr::first(static::all(), function ($post) use ($slug){
+        //     return $post['slug'] == $slug;
+        // });
+
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+        
+        if(!$post) {
+            abort(404);
+        }
+        
+        return $post;
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -12,61 +13,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Artikel 1',
-            'author' => 'Fauzi R',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Iusto et nobis soluta provident autem repellendus nemo, 
-            nesciunt delectus veritatis reprehenderit minima maxime 
-            impedit dolorum rerum ipsam repellat assumenda. 
-            Recusandae, architecto?'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Artikel 2',
-            'author' => 'Fauzi R',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Iusto et nobis soluta provident autem repellendus nemo, 
-            nesciunt delectus veritatis reprehenderit minima maxime 
-            impedit dolorum rerum ipsam repellat assumenda. 
-            Recusandae, architecto?'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Artikel 1',
-            'author' => 'Fauzi R',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Iusto et nobis soluta provident autem repellendus nemo, 
-            nesciunt delectus veritatis reprehenderit minima maxime 
-            impedit dolorum rerum ipsam repellat assumenda. 
-            Recusandae, architecto?'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Artikel 2',
-            'author' => 'Fauzi R',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Iusto et nobis soluta provident autem repellendus nemo, 
-            nesciunt delectus veritatis reprehenderit minima maxime 
-            impedit dolorum rerum ipsam repellat assumenda. 
-            Recusandae, architecto?'
-        ]
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($slug){
-        return $post['slug'] == $slug;
-    });
+    $post =  Post::find($slug);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
