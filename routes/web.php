6126f7a77f2c9a93e9400adccd59ad8ecc35bc0b
Fauzi Ramdhani <fauzi.thc@gmail.com>
2024-11-07 19:25:11 +0700

    efesiensi query pakai eager loading

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,19 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Fauzi Ramdhani',  'title' => 'About Page']);
 });
 
+// Route::get('/posts', function () {
+//     return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
+// });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
+    //$post = Post::all();
+
+    //eager load
+    // $post = Post::with(['author','category'])->latest()->get();
+
+    $post = Post::latest()->get();
+
+    
+    return view('posts', ['title' => 'Blog', 'posts' => $post ]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -24,11 +35,16 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    //dipakai untuk lazy eager load. tanpa ini juga bisa
+    // $posts = $user->posts->load('category', 'author');
 
     return view('posts', ['title' => count($user->posts) . ' ' . 'Articles By ' . $user->name , 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    //dipakai untuk lazy eager load. tanpa ini juga bisa
+    // $posts = $category->posts->load('category', 'author');
+
 
     return view('posts', ['title' => 'Articles in ' . $category->name , 'posts' => $category->posts]);
 });
