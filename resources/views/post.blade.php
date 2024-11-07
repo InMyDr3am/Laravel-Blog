38cd2dcb3fba4d101f04da66f5073d9593693f45
Fauzi Ramdhani <fauzi.thc@gmail.com>
2024-11-07 15:02:43 +0700

    membuat post berdasar author, route baru

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
         <h2 class="mb-1 text-3xl tracking-tight font-bold texxt-gray-900">{{ $post['title'] }}</h2>
         
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 1 januari 2024
+            <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | 1 januari 2024
         </div>
         <p class="my-4 font-light">
             {{ $post['body'] }}
